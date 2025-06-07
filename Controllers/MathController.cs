a27ca8407dec3cc04a44df514c64e2d5ba9cd429
Ilya4rh <chernykn12ilya12@gmail.com>
2025-06-07 14:33:22 +0500

    init

---
+++ Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevopsAPI.Controllers;
+
+[Route("api/math")]
+public class MathController : ControllerBase
+{
+    [HttpPost("sum")]
+    public IActionResult Sum([FromBody]NumRequest request)
+    {
+        var result = request.Number1 + request.Number2;
+
+        return Ok(result);
+    }
+}
---
+++ Controllers/NumRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DevopsAPI.Controllers;
+
+public record NumRequest
+{
+    public required int Number1 { get; init; }
+    
+    public required int Number2 { get; init; }
+}
