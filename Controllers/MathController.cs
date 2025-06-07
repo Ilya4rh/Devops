9cb8e70ef861b8e6ac7244a092ccaecbc35d3ba1
Ilya4rh <chernykn12ilya12@gmail.com>
2025-06-07 18:11:57 +0500

    Добавлен эндпоинт деления

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -28,4 +28,17 @@ public class MathController : ControllerBase
 
         return Ok(result);
     }
+    
+    [HttpPost("divide")]
+    public IActionResult Divide([FromBody]NumRequest request)
+    {
+        if (request.Number2 == 0)
+        {
+            return BadRequest("На ноль делить нельзя");
+        }
+        
+        var result = request.Number1 / request.Number2;
+
+        return Ok(result);
+    }
 }
